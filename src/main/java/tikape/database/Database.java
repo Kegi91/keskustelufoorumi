907d43fd86c278a848a7c078401cdf647519b361
Juho Kuisma <kujuku@dhcp-asv-95.helsinki.fi>
2016-07-01 04:02:42 +0300

    Heroku test

--- src/main/java/tikape/database/Database.java
+++ src/main/java/tikape/database/Database.java
@@ -64,13 +64,22 @@ public class Database {
         lista.add("DROP TABLE Viestiketju;");
         lista.add("DROP TABLE Viesti;");
 
-        lista.add("CREATE TABLE Alue (tunnus SERIAL PRIMARY KEY, nimi varchar(50));");
-        lista.add("CREATE TABLE Viestiketju (tunnus SERIAL PRIMARY KEY, "
-                + "alue INTEGER REFERENCES Alue (tunnus), ostikko varchar(50), "
-                + "luomisaika TIMESTAMP);");
-        lista.add("CREATE TABLE Viesti (tunnus SERIAL PRIMARY KEY, "
-                + "ketju INTEGER REFERENCES Viestiketju (tunnus), kayttaja varchar(50), "
-                + "luomisaika TIMESTAMP, sisalto varchar(500));");
+        lista.add("CREATE TABLE Alue ("
+                + "tunnus SERIAL PRIMARY KEY, "
+                + "nimi varchar(50)"
+                + ");");
+        lista.add("CREATE TABLE Viestiketju ("
+                + "tunnus SERIAL PRIMARY KEY, "
+                + "alue INTEGER REFERENCES Alue (tunnus), "
+                + "ostikko varchar(50), "
+                + "luomisaika TIMESTAMP"
+                + ");");
+        lista.add("CREATE TABLE Viesti ("
+                + "tunnus SERIAL PRIMARY KEY, "
+                + "ketju INTEGER REFERENCES Viestiketju (tunnus), "
+                + "kayttaja varchar(50), "
+                + "luomisaika TIMESTAMP, sisalto varchar(500)"
+                + ");");
 
         lista.add("INSERT INTO Alue (nimi) VALUES ('Ohjelmointi');");
         lista.add("INSERT INTO Alue (nimi) VALUES ('Musiikki');");
@@ -82,7 +91,44 @@ public class Database {
 
         lista.add("INSERT INTO Viesti (ketju, kayttaja, sisalto, luomisaika) "
                 + "VALUES (1, 'Kegi', 'Java ei oo kivaa vaan siedettävää', current_timestamp(2));");
-        
+
+        return lista;
+    }
+    
+    private List<String> sqliteLauseet() {
+        ArrayList<String> lista = new ArrayList<>();
+
+        lista.add("CREATE TABLE Alue ("
+                + "tunnus integer PRIMARY KEY, "
+                + "nimi varchar(50)"
+                + ");");
+        lista.add("CREATE TABLE Viestiketju ("
+                + "tunnus integer PRIMARY KEY, "
+                + "alue INTEGER, "
+                + "ostikko varchar(50), "
+                + "luomisaika DATETIME, "
+                + "FOREIGN KEY(alue) REFERENCES Alue(tunnus)"
+                + ");");
+        lista.add("CREATE TABLE Viesti ("
+                + "tunnus SERIAL PRIMARY KEY, "
+                + "ketju INTEGER, "
+                + "kayttaja varchar(50), "
+                + "luomisaika DATETIME, "
+                + "sisalto varchar(500), "
+                + "FOREIGN KEY ketju REFERENCES Viestiketju(tunnus)"
+                + ");");
+
+        lista.add("INSERT INTO Alue (nimi) VALUES ('Ohjelmointi');");
+        lista.add("INSERT INTO Alue (nimi) VALUES ('Musiikki');");
+        lista.add("INSERT INTO Alue (nimi) VALUES ('Elokuvat');");
+        lista.add("INSERT INTO Alue (nimi) VALUES ('Muut');");
+
+        lista.add("INSERT INTO Viestiketju(alue, otsikko, luomisaika) "
+                + "VALUES (1, 'Java', DATETIME('now','local'));");
+
+        lista.add("INSERT INTO Viesti (ketju, kayttaja, sisalto, luomisaika) "
+                + "VALUES (1, 'Kegi', 'Java ei oo kivaa vaan siedettävää', DATETIME('now', 'local'));");
+
         return lista;
     }
 
